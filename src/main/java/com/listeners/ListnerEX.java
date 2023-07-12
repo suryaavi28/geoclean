85caabb3e7ce09af70e9365f20d0f485f41e62dd
suryaavi28 <suryaavi28@gmail.com>
2023-07-12 21:22:04 +0530

    all

---
+++ src/main/java/com/listeners/ListnerEX.java
@@ -0,0 +1,54 @@
+package com.listeners;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class ListnerEX implements ITestListener {
+
+	@Override
+	public void onTestStart(ITestResult result) {
+		
+	}
+
+	
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+
+	}
+
+
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+	
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+	
+	}
+
+	@Override
+	public void onTestFailedWithTimeout(ITestResult result) {
+	
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		
+
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		
+	}
+
+}
